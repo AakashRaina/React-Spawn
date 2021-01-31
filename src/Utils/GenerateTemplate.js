2e145eea23c1cbd3dabafb16854174b9c97e2274
AakashRaina <aakashraina9@gmail.com>
2021-01-31 17:23:53 +0530

    [Refactor] Minor change to template generation

--- src/Utils/GenerateTemplate.js
+++ src/Utils/GenerateTemplate.js
@@ -5,27 +5,24 @@ const process = require("process");
 const ignoreFilesAndDirs = ["node_modules", "package-lock.json"];
 
 function copyDirectoryContents(src, dest) {
-  return fs.readdir(
-    src,
-    { encoding: "utf-8", withFileTypes: true },
-    (err, files) => {
-      files.forEach((f) => {
-        if (f.isFile() && !ignoreFilesAndDirs.includes(f.name)) {
-          // open file, read it and write it to the dest folder
-          try {
-            const data = fs.readFileSync(path.join(src, f.name), "utf8");
-            fs.writeFileSync(path.join(dest, f.name), data, { flag: "w+" });
-          } catch (err) {
-            console.error(err);
-          }
-        }
+  const files = fs.readdirSync(src, { encoding: "utf-8", withFileTypes: true });
 
-        if (f.isDirectory()) {
-          // call recursively
-        }
-      });
+  files.forEach((f) => {
+    if (f.isFile() && !ignoreFilesAndDirs.includes(f.name)) {
+      // open file, read it and write it to the dest folder
+      try {
+        const data = fs.readFileSync(path.join(src, f.name), "utf8");
+        fs.writeFileSync(path.join(dest, f.name), data, { flag: "w+" });
+      } catch (err) {
+        console.error(err);
+      }
     }
-  );
+
+    if (f.isDirectory()) {
+      // call recursively
+    }
+  });
+  return;
 }
 
 module.exports = {
