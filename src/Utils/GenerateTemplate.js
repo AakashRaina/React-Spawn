b62c8beb1dc67b031eb25e98f156b0483a762a39
AakashRaina <aakashraina9@gmail.com>
2021-01-31 16:45:41 +0530

    [Chore:] Completes moving files to new project folder

--- src/Utils/GenerateTemplate.js
+++ src/Utils/GenerateTemplate.js
@@ -5,13 +5,27 @@ const process = require("process");
 const ignoreFilesAndDirs = ["node_modules", "package-lock.json"];
 
 function copyDirectoryContents(src, dest) {
-  fs.readdir(src, { encoding: "utf-8", withFileTypes: true }, (err, files) => {
-    files.forEach((f) => {
-      if (f.isFile() && !ignoreFilesAndDirs.includes(f.name)) {
-        console.log(f.name);
-      }
-    });
-  });
+  return fs.readdir(
+    src,
+    { encoding: "utf-8", withFileTypes: true },
+    (err, files) => {
+      files.forEach((f) => {
+        if (f.isFile() && !ignoreFilesAndDirs.includes(f.name)) {
+          // open file, read it and write it to the dest folder
+          try {
+            const data = fs.readFileSync(path.join(src, f.name), "utf8");
+            fs.writeFileSync(path.join(dest, f.name), data, { flag: "w+" });
+          } catch (err) {
+            console.error(err);
+          }
+        }
+
+        if (f.isDirectory()) {
+          // call recursively
+        }
+      });
+    }
+  );
 }
 
 module.exports = {
