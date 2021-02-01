adcbbaaa60d746aeb1c1139860687dd0278e4685
AakashRaina <aakashraina9@gmail.com>
2021-02-01 15:19:04 +0530

    Chore: Finished template generation

--- src/Utils/GenerateTemplate.js
+++ src/Utils/GenerateTemplate.js
@@ -2,26 +2,27 @@ const fs = require("fs");
 const path = require("path");
 const process = require("process");
 
-const ignoreFilesAndDirs = ["node_modules", "package-lock.json"];
+const ignoreFilesAndDirs = ["node_modules", "package-lock.json", "dist"];
 
 function copyDirectoryContents(src, dest) {
-  const files = fs.readdirSync(src, { encoding: "utf-8", withFileTypes: true });
+  try {
+    const files = fs.readdirSync(src, {
+      encoding: "utf-8",
+      withFileTypes: true,
+    });
 
-  files.forEach((f) => {
-    if (f.isFile() && !ignoreFilesAndDirs.includes(f.name)) {
-      // open file, read it and write it to the dest folder
-      try {
+    files.forEach(async (f) => {
+      if (f.isFile() && !ignoreFilesAndDirs.includes(f.name)) {
+        // open file, read it and write it to the dest folder
         const data = fs.readFileSync(path.join(src, f.name), "utf8");
         fs.writeFileSync(path.join(dest, f.name), data, { flag: "w+" });
-      } catch (err) {
-        console.error(err);
+      } else if (f.isDirectory() && !ignoreFilesAndDirs.includes(f.name)) {
+        const directoryPath = path.join(dest, f.name);
+        await fs.promises.mkdir(directoryPath);
+        copyDirectoryContents(path.join(src, f.name), directoryPath);
       }
-    }
-
-    if (f.isDirectory()) {
-      // call recursively
-    }
-  });
+    });
+  } catch (error) {}
   return;
 }
 
